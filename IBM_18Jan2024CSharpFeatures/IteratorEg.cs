dd1c2e2750e4aefa55aad9ee3cb64d772fc6cebf
EZLLeno <EZLLeno@EZL-LEN>
2024-01-22 10:00:03 +0530

    demo

---
+++ IBM_18Jan2024CSharpFeatures/IteratorEg.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM_18Jan2024CSharpFeatures
+{
+    internal class IteratorEg
+    {
+
+        static void Main()
+        { 
+            MSEmployee obj=new MSEmployee();
+            foreach (string item in obj)
+            {
+                Console.WriteLine(item);
+                
+            }
+
+            Console.ReadKey(  );
+        }
+
+
+    }
+
+
+    class MSEmployee:IEnumerable
+    {
+        List<string> _lstSkills= new List<string> { "Coding","Testing" };
+
+        public IEnumerator GetEnumerator()
+        {
+            return new StringEnumerator(_lstSkills);
+
+        }
+    }
+
+    class StringEnumerator : IEnumerator
+    {
+
+        List<string> _data;
+        int _idx = -1;
+
+
+        public StringEnumerator(List<string> data)
+        {
+            _data = data;  
+        }
+
+       // public object Current => _data[_idx];
+        public object Current { get { 
+                return _data[_idx]; } } 
+        public bool MoveNext()
+        {
+            _idx++;
+
+            if (_idx > _data.Count-1) { return false; }
+            return true;
+        }
+
+        public void Reset()
+        {
+            _idx =-1;
+        }
+    }
+
+}
